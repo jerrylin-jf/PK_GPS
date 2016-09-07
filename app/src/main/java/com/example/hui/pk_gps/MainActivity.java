bed5854afcdb4a654c61231d562b7336509029e7
GrayHui <linhuihui225@gmail.com>
2016-09-07 17:40:55 +0800

    add mockLocation permission

--- app/src/main/java/com/example/hui/pk_gps/MainActivity.java
+++ app/src/main/java/com/example/hui/pk_gps/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.hui.pk_gps;
 
+import android.location.Criteria;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,11 +18,15 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public static int OVERLAY_PERMISSION_REQ_CODE = 1234;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        askForPermission();
+        mockPositionPermission();
+        Log.i("MainActivity", "after mock");
         Button start = (Button)findViewById(R.id.start_btn);
         Button end = (Button)findViewById(R.id.end_btn);
 
@@ -26,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i("MainActivity", "on Start_Click");
-                askForPermission();
                 Intent intent = new Intent(MainActivity.this, FloatGPS.class);
                 startService(intent);
                 finish();
@@ -49,8 +56,47 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                     Uri.parse("package:" + getPackageName()));
             startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
-        } else {
+        } else { }
 
+
+    }
+
+    public void mockPositionPermission() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean hasAddTestProvider = false;
+        boolean canMockPosition = (Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.ALLOW_MOCK_LOCATION, 0) != 0) || Build.VERSION.SDK_INT > 22;
+        if (canMockPosition && hasAddTestProvider == false) {
+            try {
+                String providerStr = LocationManager.GPS_PROVIDER;
+                LocationProvider provider = locationManager.getProvider(providerStr);
+                if (provider != null) {
+                    locationManager.addTestProvider(
+                            provider.getName()
+                            , provider.requiresNetwork()
+                            , provider.requiresSatellite()
+                            , provider.requiresCell()
+                            , provider.hasMonetaryCost()
+                            , provider.supportsAltitude()
+                            , provider.supportsSpeed()
+                            , provider.supportsBearing()
+                            , provider.getPowerRequirement()
+                            , provider.getAccuracy());
+                } else {
+                    locationManager.addTestProvider(
+                            providerStr
+                            , true, true, false, false, true, true, true
+                            , Criteria.POWER_HIGH, Criteria.ACCURACY_FINE);
+                }
+                locationManager.setTestProviderEnabled(providerStr, true);
+                locationManager.setTestProviderStatus(providerStr, LocationProvider.AVAILABLE,
+                        null, System.currentTimeMillis());
+
+                hasAddTestProvider = true;
+                canMockPosition = true;
+            } catch (SecurityException e) {
+                canMockPosition = false;
+            }
         }
     }
 }
